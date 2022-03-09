905c446981995ec711ca48028b2b89f7279792a1
Marcelo De Lima <delima_marcelo@outlook.com>
2022-03-09 15:24:19 -0300

    avancando

--- fundamentos-web/fundamentos/bloco-4-introducao-javascrit/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicio-01.js
+++ fundamentos-web/fundamentos/bloco-4-introducao-javascrit/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicio-01.js
@@ -1,13 +1,14 @@
 let num = 10
 let lista = []
 
-for (let i = num; i > 0; i--){
+for (let i = num; i > 0; i--) {
     lista.push(i)
 }
 
 let fator = 1
+
 for (let x = 0; x < lista.length; x++) {
-    console.log(lista.length[x]);
+    fator = fator * lista[x]
 }
 
 console.log(fator);
--- fundamentos-web/fundamentos/bloco-4-introducao-javascrit/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicio-02.js
+++ fundamentos-web/fundamentos/bloco-4-introducao-javascrit/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicio-02.js
@@ -1,13 +1,7 @@
 let word = 'tryber';
 let inverso = ""
 
-for (let i = word.length; i >= 0; i--) {
+for (let i = word.length -1 ; i >= 0; i--) {
     inverso += word[i]
 }
 console.log(inverso);
-
-
-//for (let i = 0; i < word.length; i++) {
-//  inverso += word[i]
-//}
-//console.log(inverso);
