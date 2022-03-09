88f228877865e07b9b265a21f1ef46b36a683432
Marcelo De Lima <delima_marcelo@outlook.com>
2022-03-09 14:09:49 -0300

    evoluindo

---
+++ fundamentos-web/fundamentos/bloco-4-introducao-javascrit/dia-3-javascript-logica-de-programacao-e-algoritimos/exercicio-02.js
@@ -0,0 +1,13 @@
+let word = 'tryber';
+let inverso = ""
+
+for (let i = word.length; i >= 0; i--) {
+    inverso += word[i]
+}
+console.log(inverso);
+
+
+//for (let i = 0; i < word.length; i++) {
+//  inverso += word[i]
+//}
+//console.log(inverso);
