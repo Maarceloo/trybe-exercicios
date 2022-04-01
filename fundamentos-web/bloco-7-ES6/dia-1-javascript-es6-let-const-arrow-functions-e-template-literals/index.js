05c35d708df7c57a2da9027e7cc5d4e7d22e0ea6
Marcelo De Lima <delima_marcelo@outlook.com>
2022-04-01 19:22:37 -0300

    part 1

---
+++ fundamentos-web/bloco-7-ES6/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
@@ -0,0 +1,29 @@
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     const elseScope = "Não devo ser utilizada fora meu escopo (else)";
+//     console.log(elseScope);
+//   }
+// //   console.log(ifScope + " o que estou fazendo aqui ? :O"); // Se necessário esta linha pode ser removida.
+// };
+
+// testingScope(true);
+
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+// // Seu código aqui.
+
+// // crescente
+// oddsAndEvens.sort((a, b) => a - b);
+// console.log(oddsAndEvens);
+
+// // decrescente
+// oddsAndEvens.sort((a, b) => b - a);
+// console.log(oddsAndEvens);
+
+
+// ADICIONAIS
+
