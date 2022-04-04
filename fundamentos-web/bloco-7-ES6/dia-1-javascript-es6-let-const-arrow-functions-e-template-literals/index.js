a27a75a47bed6c4a2bc0a0465f434d2998db73dc
Marcelo De Lima <delima_marcelo@outlook.com>
2022-04-04 19:03:43 -0300

    bloco 7 try/catch

--- fundamentos-web/bloco-7-ES6/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
+++ fundamentos-web/bloco-7-ES6/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/index.js
@@ -1,29 +1,10 @@
-// const testingScope = (escopo) => {
-//   if (escopo === true) {
-//     let ifScope = "Não devo ser utilizada fora do meu escopo (if)";
-//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-//     console.log(ifScope);
-//   } else {
-//     const elseScope = "Não devo ser utilizada fora meu escopo (else)";
-//     console.log(elseScope);
-//   }
-// //   console.log(ifScope + " o que estou fazendo aqui ? :O"); // Se necessário esta linha pode ser removida.
-// };
-
-// testingScope(true);
-
-// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
-
-// // Seu código aqui.
-
-// // crescente
-// oddsAndEvens.sort((a, b) => a - b);
-// console.log(oddsAndEvens);
-
-// // decrescente
-// oddsAndEvens.sort((a, b) => b - a);
-// console.log(oddsAndEvens);
-
-
-// ADICIONAIS
-
+const getDayName = (number) => {
+    const dayNumber = number - 1;
+    const days = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
+    return days[dayNumber];
+  };
+  
+  const printDayname = (dayNumber) => {
+    console.log(getDayName(dayNumber));
+  };
+  
